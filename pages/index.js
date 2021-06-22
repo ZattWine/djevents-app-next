f8e229a03882be27399b5577fd85ee6efdff6922
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-06-22 14:33:42 +0630

    fetch data by sg getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   )
+}
+
+export async function getStaticProps(context) {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  if (!events) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { events },
+    revalidate: 1,
+  }
 }
