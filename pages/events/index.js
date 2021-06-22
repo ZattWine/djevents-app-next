6003fcd9203046d4c102653f3e51ecef28c4edcd
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-06-22 21:19:37 +0630

    events pagination

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 import Layout from '@/components/Layout'
+import Pagination from '@/components/Pagination'
 import { API_URL } from '@/config/index'
 import EventItem from '@/components/EventItem'
+import { PER_PAGE } from '@/config/index'
 
-export default function EventsPage({ events }) {
+export default function EventsPage({ events, page, total }) {
   return (
     <Layout>
       <h1>All Events</h1>
@@ -11,22 +14,27 @@ export default function EventsPage({ events }) {
       {events.map((evt) => (
         <EventItem key={evt.slug} evt={evt} />
       ))}
+
+      <Pagination page={page} total={total} />
     </Layout>
   )
 }
 
-export async function getStaticProps(context) {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`)
-  const events = await res.json()
+export async function getServerSideProps({ query: { page = 1 } }) {
+  // calculate start page
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE
 
-  if (!events) {
-    return {
-      notFound: true,
-    }
-  }
+  // fetch total/count
+  const totalRes = await fetch(`${API_URL}/events/count`)
+  const total = await totalRes.json()
+
+  // fetch events
+  const eventsRes = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  )
+  const events = await eventsRes.json()
 
   return {
-    props: { events },
-    revalidate: 1,
+    props: { events, page: +page, total },
   }
 }
